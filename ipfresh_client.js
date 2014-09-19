9e1f4d4dac1ae867522d5d1d7afd8b27fd439bb7
fenixA <f.assange@googlemail.com>
2014-09-19 02:15:44 +0200

    first commit

---
+++ ipfresh_client.js
@@ -0,0 +1,8 @@
+var os = require('os');
+var ws = require('ws');
+
+var websocket = new ws('ws://vertigo.canopus.uberspace.de:65432');
+
+websocket.on('open', function() {
+    websocket.sent(os.hostname());
+});
